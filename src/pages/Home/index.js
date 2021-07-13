e155b87ee3003d51fac8b24a7ac204fb27b8f93d
atila-tokyo <atilamanager@gmail.com>
2021-07-13 21:07:35 +0900

    Adding navbar and input form with some styling

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,13 +1,28 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Button, Card } from 'react-bootstrap';
+import { Navbar, Button, Card, Form, InputGroup, FormControl } from 'react-bootstrap';
 
 function App(props) {
 
     return (
         <>
-        <Card></Card>
-        <Button>Se inscreva!</Button>
+        <Navbar bg="dark" variant="dark">
+            <Form inline>
+                <InputGroup>
+                    <InputGroup.Prepend>
+                    <InputGroup.Text id="basic-addon1">@</InputGroup.Text>
+                    </InputGroup.Prepend>
+                    <FormControl
+                    placeholder="E-mail"
+                    aria-label="Username"
+                    aria-describedby="basic-addon1"
+                    />
+                </InputGroup>
+            </Form>
+            <Form inline>
+            <Button>Se inscreva!</Button>
+            </Form>
+            </Navbar>
         </>
     );
 }
