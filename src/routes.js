8f8e414f734965e9613da0cc7a177eae79640da8
atila-tokyo <atilamanager@gmail.com>
2021-07-13 16:40:05 +0900

    Adding Routes file config

--- src/routes.js
+++ src/routes.js
@@ -1,1 +1,15 @@
 import React from 'react';
+import { Switch, Route, BrowserRoute } from 'react-router-dom';
+import Home from './pages/Home';
+import Subscribed from './pages/Subscribed';
+
+export default function Routes() {
+    return (
+        <BrowserRoute>
+            <Switch>
+                <Route path='/' exact component={Home} />
+                <Route path='/subscribed' component={Subscribed} />
+            </Switch>
+        </BrowserRoute>
+    )
+}
