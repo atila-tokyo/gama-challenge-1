8e9ff5cc79a902c7aab0d174ccaafafdb79ac5a1
atila-tokyo <atilamanager@gmail.com>
2021-07-13 20:26:40 +0900

    Adding button and dependencies

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,1 +1,15 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button, Card } from 'react-bootstrap';
+
+function App(props) {
+
+    return (
+        <>
+        <Card></Card>
+        <Button>Se inscreva!</Button>
+        </>
+    );
+}
+
+export default App;
--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import { Switch, Route, BrowserRoute } from 'react-router-dom';
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
 import Home from './pages/Home';
 import Subscribed from './pages/Subscribed';
 
 export default function Routes() {
     return (
-        <BrowserRoute>
+        <BrowserRouter>
             <Switch>
                 <Route path='/' exact component={Home} />
                 <Route path='/subscribed' component={Subscribed} />
             </Switch>
-        </BrowserRoute>
+        </BrowserRouter>
     )
 }
