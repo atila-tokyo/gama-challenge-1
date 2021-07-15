0ac8adff210d0b1926cbe7a94cc55b4afd4c6227
atila-tokyo <atilamanager@gmail.com>
2021-07-15 11:30:12 +0900

    Adding last detail on footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,7 +5,7 @@ export default function Footer() {
         <div className="page-footer font-small pt-2 pb-2" bg="dark">
             <div className="row">
                 <div className="col-md-4"></div>
-                <div className="col-md-4 text-center">&copy; 2021. Todos os direitos reservados</div>
+                <div className="col-md-4 text-center">Originals Collectibles &copy; 2021. Todos os direitos reservados</div>
                 <div className="col-md-4"></div>
             </div>
         </div>
